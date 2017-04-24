6b962bea86445d95798ec6ac74e8dde84ed3979c
aamc4303 <aamc4303@colorado.edu>
2017-04-24 10:52:41 -0600

    Added notes to main.c

---
+++ Code/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void main(int argc, char *argv[]){
+
+// Read in data
+
+// Build the structs based on the data. Probably 1 struct per ticker name
+
+// Find NASDAQ or S&P500 or something  and keep that separate so that you can use it as the market baseline
+
+// Pass 50 days of market baseline as well as the 50 corresponding days of your specific stock
+
+}
