ae63e4e57d188f3fdd58d9e7cb122b19ecb528d9
aamc4303 <aamc4303@colorado.edu>
2017-04-26 09:28:05 -0600

    Clarified comments

--- Code/main.c
+++ Code/main.c
@@ -9,8 +9,13 @@ void main(int argc, char *argv[]){
 
 // Build the structs based on the data. Probably 1 struct per ticker name
 
-// Find NASDAQ or S&P500 or something  and keep that separate so that you can use it as the market baseline
+// Find NASDAQ or S&P500 or something and keep that separate so that you can use it as the market baseline
 
-// Pass 50 days of market baseline as well as the 50 corresponding days of your specific stock
+// Pass 50 days of market baseline as well as the 50 corresponding days of your specific stock to a decision making function
 
+// Recieve an ALPHA value from the decision making function. Store the alpha value and toss EVERYTHING ELSE to conserve memory. Maybe keep a count of how many alpha values are stored (for the purpose of averages)
+
+// Compute the average alpha value
+
+// Jump off a building when you see average alpha value
 }
