ed4c35a491566ef69bd2b13dbea099cf77fae8db
amuesing1 <amuesing1@gmail.com>
2017-04-26 11:10:10 -0600

    quick fix

--- Code/beta.c
+++ Code/beta.c
@@ -4,7 +4,7 @@
 double beta(double stock_start, double SPY_start, double stock_end, double SPY_end) {
   double beta,stock_change,SPY_change;
   stock_change=stock_end-stock_start;
-  SPY_change=SPY_end=SPY_start;
+  SPY_change=SPY_end-SPY_start;
   beta=stoc_changek/SPY_change;
   return beta;
 }
