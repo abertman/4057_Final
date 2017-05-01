e95a9cca91951557fca72a19233fb0494bd1e2bb
aamc4303 <aamc4303@colorado.edu>
2017-05-01 09:32:55 -0600

    ReadFile now working

--- Code/ReadFile.c
+++ Code/ReadFile.c
@@ -6,8 +6,6 @@ double *ReadFile(char Ticker[],int number_of_rows){
 
 char filename[20];
 
-char date[20];
-
 snprintf(filename,20,"%s.csv",Ticker);
 
 FILE *filehandle = fopen(filename,"r");
@@ -19,10 +17,7 @@ double holder;
 double *Price = (double *)malloc(sizeof(double *)*number_of_rows);
 
 for (i = 0; i < number_of_rows; i++){
-	printf("%d\n",i);
-//	fscanf(filehandle,"%lf",&Price[i]);
-	fscanf(filehandle,"%*[^ ,]%lf",&holder);
-//	printf("%f\n",holder);
+	fscanf(filehandle,"%*f%*c%*f%*c%*f%*c%lf",&Price[i]);
 }
 
 fclose(filehandle);
