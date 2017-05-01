27daa816ac7d3f2dd76a3c0f83b3ca381f1f9675
amuesing1 <amuesing1@gmail.com>
2017-05-01 10:21:27 -0600

    Added output

--- Code/main.c
+++ Code/main.c
@@ -5,6 +5,7 @@
 #include "ReadFile.h"
 #include "beta.h"
 #include "alpha.h"
+#include "Output.h"
 
 int main(int argc, char *argv[]){
 
@@ -21,6 +22,7 @@ printf("Stock prices collected\n");
 int i,j = 0;
 
 int *Good_i = (int *)malloc(sizeof(int *)*(number_of_rows/25));
+int *all_beta = (int *)malloc(sizeof(int *)*(number_of_rows/25));
 
 double Beta, Alpha;
 
@@ -28,20 +30,24 @@ for (i = 0; i < (number_of_rows/25); i++){
 	Beta = beta(Price[i*24],BaselinePrice[i*24],Price[i*24 + 24],BaselinePrice[i*24 + 24]);
 	if ((Price[i*24 + 24]-Price[i*24]) > 0 && abs(Beta) > 1){
 		Good_i[j] = i;
+		all_beta[j]=Beta;
+		// printf("%d\n",i );
 		j++;
 		// You've decided to buy the stock if these two conditions are met (ie it's trending up and it has a "large" beta value)
 	}
 }
 
 i = 0;
+double *all_alpha = (double *)malloc(sizeof(double *)*j);
+while (i < j) {
 
-while (i <= j) {
-	
 	Alpha = alpha(Price[Good_i[i]*24],BaselinePrice[Good_i[i]*24],Price[Good_i[i]*24 + 24],BaselinePrice[Good_i[i]*24 + 24]);
+	all_alpha[i]=Alpha;
 	// Store Alpha somewhere
 	i++;
 }
 
+Output("AAPL",j,Good_i,all_beta,all_alpha);
 	// Find the trend of the stock
 	// Send trend and beta value to a decision making function (returns 1 or 0)
 	// If the decision is a BUY (1), see how the stock does the next month within function alpha.c
